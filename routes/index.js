779ab7fe12fd958ee82bef1e44636c8f3589daa7
ultrawide <colinchan02@gmail.com>
2020-05-09 15:44:19 -0700

    Add submit message feature and validator

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const validator = require('express-validator');
+
 const messages = [
   {
     text: "Hi there!",
@@ -17,6 +19,46 @@ const messages = [
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+/* GET form. */
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: "Create Message"});
+});
+
+/* POST form. */
+router.post('/new', [
+  validator.body('user', 'User must not be empty.').trim().isLength({ min: 1 }),
+  validator.body('message', 'Message must not be empty.').trim().isLength({ min: 1 }),
+  validator.sanitizeBody('*').escape()
+], function(req, res, next) {
+  // Validate fields.
+  validator.body('user', 'User must not be empty.').trim().isLength({ min: 1 });
+  validator.body('message', 'Message must not be empty.').trim().isLength({ min: 1 });
+
+  // Sanitize fields (using wildcard)
+  validator.sanitizeBody('*').escape();
+
+  // Extract the validation errors from a request.
+  const errors = validator.validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // There are errors. Render the form again with sanitized values/error messages.
+    res.render('form', { title: 'Create Message', errors: errors.array()});
+    return;
+  }  
+
+  // Create a message object with escaped and trimmed data
+  var message = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  }
+
+  messages.push(message);
+
+  res.redirect('/');
+
 });
 
 module.exports = router;
